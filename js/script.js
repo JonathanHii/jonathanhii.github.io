c0182de6317c6ab5b92970557503a4fb36f84353
Jonathan Hii <158047666+JonathanHii@users.noreply.github.com>
2025-01-30 21:07:52 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function showDropdown() {
+    const dropdown = document.querySelector("#dropdown-menu")
+    dropdown.style.display = 'flex'
+}
+
+function hideDropdown() {
+    const dropdown = document.querySelector("#dropdown-menu")
+    dropdown.style.display = 'none'
+}
+
+
+
