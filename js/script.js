6cfb86f7f35cfe3e815891c36731375e0b705400
Jonathan <jhii2@outlook.com.au>
2025-02-01 13:21:12 +1000

    remove typing

--- js/script.js
+++ js/script.js
@@ -10,24 +10,6 @@ function hideDropdown() {
 }
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const text = "Hi! I am Jonathan!";
-    let index = 0;
-    const speed = 100; // typing speed in milliseconds
-
-    function typeEffect() {
-        if (index < text.length) {
-            document.getElementById("typing-effect").innerHTML = text.substring(0, index + 1);
-            index++;
-            setTimeout(typeEffect, speed);
-        }
-    }
-
-    typeEffect();
-});
-
-
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("navbar-menu");
     if (window.scrollY > 50) {
