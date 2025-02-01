f98d621a344a48b1ee32288332d75e3a15e83614
Jonathan <jhii2@outlook.com.au>
2025-02-01 15:12:21 +1000

    change animation

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function hideDropdown() {
 
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("navbar-menu");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 150) {
         navbar.classList.add("shrink");
     } else {
         navbar.classList.remove("shrink");
