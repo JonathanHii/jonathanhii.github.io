7891363347fac47121c22950affe19202be9fc30
Jonathan <jhii2@outlook.com.au>
2025-02-16 17:00:03 +1000

    add animation

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ function hideDropdown() {
     dropdown.style.display = 'none';
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    const text = "Hi! I am Jonathan!";
+    let index = 0;
+    const speed = 100; // typing speed in milliseconds
+    function typeEffect() {
+        if (index < text.length) {
+            document.getElementById("typing-effect").innerHTML = text.substring(0, index + 1);
+            index++;
+            setTimeout(typeEffect, speed);
+        }
+    }
+    typeEffect();
+});
+
 
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("navbar-menu");
